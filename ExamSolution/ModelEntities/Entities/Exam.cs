9892da74df058a84c1799ceb8aaa8c5557182c1e
EarlyDurban <soulfly.jah@yandex.ru>
2015-02-02 00:16:59 +0200

    1

--- ExamSolution/ModelEntities/Entities/Exam.cs
+++ ExamSolution/ModelEntities/Entities/Exam.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,10 @@ namespace ModelEntities.Entities
 {
     public class Exam
     {
+        [Key]
+        public int ExamID { get; set; }
+
+        [MaxLength(250)]
+        public string Name { get; set; }
     }
 }
